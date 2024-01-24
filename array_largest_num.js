f5d090f99d5e988e89281dc6c6e5d4c9089f0bb9
kajal <122118273+Kajalshukla-ka-i@users.noreply.github.com>
2024-01-24 16:11:25 +0530

    Create array_largest_num.js

---
+++ array_largest_num.js
@@ -0,0 +1,25 @@
+<script>
+
+    // Algorithm
+// =================================================
+document.write()
+    // Input: An array of numbers, let's call it arr.
+
+    // Output: The largest number in the array.
+
+    // Initialize a variable:
+
+    // Set largest to the first element of the array (arr[0]).
+    // Iterate through the array:
+
+    // Start a loop from index 1 to the end of the array.
+    // For each element at index i:
+    // Compare arr[i] with the current value of largest.
+    // If arr[i] is greater than largest, update largest to be equal to arr[i].
+    // Return the result:
+
+    // After the loop is complete, largest will contain the largest number in the array.
+    // Return the value of largest as the result.
+
+
+</script>
